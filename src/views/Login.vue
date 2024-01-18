be292109fe8601180a317da930c1da727f17a009
YALNEZAMI <yaseralnezami@gmail.com>
2024-01-18 02:23:29 +0100

    set code in local storage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -93,7 +93,8 @@ if (store.state.ENV === "production") {
   clientId = "e2e8ff7af3a5438384f5cc8d3b871aae"; // prod
 }
 
-const code = route.query.code;
+const code = localStorage.getItem("code") || route.query.code;
+localStorage.setItem("code", "code");
 //fonction executé au montage
 onMounted(async () => {
   //si pas de code, on reste sur la page login
